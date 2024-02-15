0002689252409ec81fa2a30a2a6e6a6013d85df8
Owen Grimm <ozgrimm@uwaterloo.ca>
2024-02-15 17:32:30 -0500

    Have something working 🎉🎉

--- src/QuadratureEncoder.cpp
+++ src/QuadratureEncoder.cpp
@@ -1,12 +1,109 @@
 #include "QuadratureEncoder.hpp"
 #include <Arduino.h>
 
-#define EDGES_PER_PULSE 4
+/* ============= CONFIG HERE ============= */
 
-using QuadratureEncoder::impl::QuadratureEncoderClass;
+#define ENCODER_SIGNAL_A_PIN 2
+#define ENCODER_SIGNAL_A_INT_VECTOR INT0_vect
+#define ENCODER_SIGNAL_B_PIN 3
+#define ENCODER_SIGNAL_B_INT_VECTOR INT1_vect
+#define ENCODER_SIGNAL_IN_PORT PIND
+#define ENCODER_PULSES_PER_REVOLUTION 3600
+
+/* ======================================= */
+
+// Don't change these ones tho...
+#define EDGES_PER_PULSE 2
+
+// Unfortunately, because there's no good way to grab pin ports registers,
+// check their interrupt vectors, or generate ISRs (without Arduino's high-
+// latency `attachInterrupt()`), information on all of these must be passed to
+// the library via compile-time known `#define`s to ensure that this library
+// works quickly. It is the user's responsibility to ensure all related 
+// `#define`s are made correctly to ensure correct funtionality.
+//
+// For descriptions of each of these required `#defines`, see below.
+#if defined(ENCODER_SIGNAL_A_PIN)           && \
+    defined(ENCODER_SIGNAL_A_INT_VECTOR)    && \
+    defined(ENCODER_SIGNAL_B_PIN)           && \
+    defined(ENCODER_SIGNAL_B_INT_VECTOR)    && \
+    defined(ENCODER_SIGNAL_IN_PORT)
+
+#if digitalPinToInterrupt(ENCODER_SIGNAL_A_PIN) == NOT_AN_INTERRUPT
+    #error "Signal A pin must be interrupt-enabled."
+#endif
+#if digitalPinToInterrupt(ENCODER_SIGNAL_B_PIN) == NOT_AN_INTERRUPT
+    #error "Signal B pin must be interrupt-enabled."
+#endif
+
+#define ISR_COMPILABLE
+
+#else
+
+// `ENCODER_SIGNAL_A_PIN` should be the Arduino digital pin number of the
+// digital pin to which the "A" signal of the quadrature encoder is attached.
+// Example: `#define ENCODER_SIGNAL_A_PIN 2`
+#ifndef ENCODER_SIGNAL_A_PIN
+    #error "`ENCODER_WIRE_A_PIN` must be defined for library use."
+#endif
+
+// `ENCODER_SIGNAL_B_PIN` should be the Arduino digital pin number of the
+// digital pin to which the "B" signal of the quadrature encoder is attached.
+// Example: `#define ENCODER_SIGNAL_B_PIN 3`
+#ifndef ENCODER_SIGNAL_B_PIN
+    #error "`ENCODER_WIRE_B_PIN` must be defined for library use."
+#endif
+
+// `ENCODER_SIGNAL_A_INT_VECTOR` should be the interrupt vector of the on-change
+// interrupt of the digital pin to which the "A" signal of the quadrature
+// encoder is attached. This MUST be the interrupt vector for the corresponding
+// pin otherwise this library will not work.
+// Example: `#define ENCODER_SIGNAL_A_INT_VECTOR INT0_vect`
+#ifndef ENCODER_SIGNAL_A_INT_VECTOR
+    #error "`ENCODER_SIGNAL_A_INT_VECTOR must be defined for library use"
+
+#endif
+// `ENCODER_SIGNAL_B_INT_VECTOR` should be the interrupt vector of the on-change
+// interrupt of the digital pin to which the "B" signal of the quadrature
+// encoder is attached. This MUST be the interrupt vector for the corresponding
+// pin otherwise this library will not work.
+// Example: `#define ENCODER_SIGNAL_B_INT_VECTOR INT1_vect`
+#ifndef ENCODER_SIGNAL_B_INT_VECTOR
+    #error "`ENCODER_SIGNAL_B_INT_VECTOR must be defined for library use"
+#endif
+
+// `ENCODER_SIGNAL_IN_PORT` should be the Portx input register for the digital
+// pins to which both the "A" and "B" signals of the quadrature encoder are
+// attached. These two pins MUST be on the same port to allow the fastest
+// possible interrupt latency and avoid missing pulses. This MUST be the port to
+// which BOTH encoder signal wires are attached, otherwise this library will not
+// work.
+// Example: `#define ENCODER_SIGNAL_IN_PORT PIND`
+#ifndef ENCODER_SIGNAL_IN_PORT
+    #error "`ENCODER_SIGNAL_IN_PORT` must be defined for library use."
+#endif
+
+#endif
+
+// `ENCODER_PULSES_PER_REVOLUTION` should be the number of pulses generated per
+// revolution by the attached quadrature encoder. Note that this library uses 
+// rising and falling edges of the A and B signals of the encoder, meaning that
+// angular displacement will be measured 4x more frequently than the specified
+// PPR value.
+#ifndef ENCODER_PULSES_PER_REVOLUTION
+    #error "`ENCODER_PULSES_PER_REVOLUTION` must be defined for library use."
+#endif
+
+// `ENCODER_REVERSE` should be defined if the reported angular displacement of
+// the encoder should be inverted (+/- movement directions swapped).
+#ifdef ENCODER_REVERSE
+#define __ENCODER_SWAP_WIRES 1
+#else
+#define __ENCODER_SWAP_WIRES 0
+#endif
 
 /**
- * \brief Constructor for the EncoderClass class.
+ * \brief Constructor for the QuadratureEncoderClass class.
  * 
  * @param pin_a The digital pin number of the pin connected to wire A of the
  * encoder. Must be interrupt-enabled and belonging to the same port as pin_b.
@@ -18,23 +115,18 @@ using QuadratureEncoder::impl::QuadratureEncoderClass;
  * @param invert_direction Whether or not to invert the reported direction of
  * the motor.
 */
-QuadratureEncoderClass::QuadratureEncoderClass(
-    uint8_t pin_a,
-    uint8_t pin_b,
-    uint16_t pulses_per_revolution,
-    bool invert_direction = false
-) {
-    this->ppr = pulses_per_revolution;
-    this->epr = (double) pulses_per_revolution * EDGES_PER_PULSE;
+QuadratureEncoder::QuadratureEncoder() {
+    this->ppr = ENCODER_PULSES_PER_REVOLUTION;
+    this->epr = (double) (this->ppr * EDGES_PER_PULSE);
     this->pulses = 0;
 
-    uint8_t pin_a_bm = digital_pin_to_bit_mask_PGM[pin_a];
-    uint8_t pin_b_bm = digital_pin_to_bit_mask_PGM[pin_b];
+    uint8_t pin_a_bm = digital_pin_to_bit_mask_PGM[ENCODER_SIGNAL_A_PIN];
+    uint8_t pin_b_bm = digital_pin_to_bit_mask_PGM[ENCODER_SIGNAL_B_PIN];
 
     // By treating pin_x as wire A input and pin_y as wire B input, we can swap
     // the direction reported by the encoder by swapping which pin is assigned
     // to our logical pins.
-    if (invert_direction) {
+    if (__ENCODER_SWAP_WIRES) {
         this->pin_x_bm = pin_b_bm;
         this->pin_y_bm = pin_a_bm;
     } else {
@@ -43,29 +135,32 @@ QuadratureEncoderClass::QuadratureEncoderClass(
     }
 
     // This is an extraoirdinarily hacky way to do this but I just want to get
-    // this library up and running ASAP
+    // this library up and running tbh
     #ifdef __AVR_ATmega328P__
-        // Setting interrupts 0 and 1 to be triggered on any logic level change, then
-        // enabling them.
+        // Since there are only two interrupt-enabled GPIO pins on the
+        // ATmega328p, it is "safe" to assume that both are being used by this
+        // library. 
+        // Setting interrupts 0 and 1 to be triggered on any logic level change,
+        // then enabling them.
         // See documentation for EICRA and EIMSK registers to learn more
-        EICRA = 0b00000101;
-        EIMSK = 0b00000011;
+        EICRA = 0b00000101;     // Setting int0 and int1 to on-change trigger
+        EIMSK = 0b00000011;     // Enabling int0 and int1
     #else
         #error "Unsupported MCU. Only the ATmega328p is currently supported"
     #endif
 
     // Using Arduino's slow `pinMode()` here is fine since this constructor will
     // only be called once
-    pinMode(pin_a, INPUT_PULLUP);
-    pinMode(pin_b, INPUT_PULLUP);
+    pinMode(ENCODER_SIGNAL_A_PIN, INPUT_PULLUP);
+    pinMode(ENCODER_SIGNAL_B_PIN, INPUT_PULLUP);
 
 }
 
 /**
- * Gets the current reported angle of the encoder in fractional .
+ * Gets the current reported angle of the encoder in fractional rotations.
 */
-QuadratureEncoder::AngleType QuadratureEncoderClass::getAngle() {
-    return this->pulses / this->epr;
+QuadratureEncoder::AngleType QuadratureEncoder::getAngle() {
+    return this->pulses / (this->epr);
 };
 
 /**
@@ -78,16 +173,17 @@ QuadratureEncoder::AngleType QuadratureEncoderClass::getAngle() {
  * A blocking function which finds the average velocity by taking the initial
  * angle, 
 */
-QuadratureEncoder::AngleType QuadratureEncoderClass::pollAngularVelocity(
+QuadratureEncoder::AngleType QuadratureEncoder::pollAngularVelocity(
     uint32_t poll_duration
 ) {
-    // Getting initial duration
+    // Getting initial pos
     QuadratureEncoder::AngleType angle_initial = this->getAngle();
-
+    // Waiting
     delay(poll_duration);
-
+    // Getting final pos
     QuadratureEncoder::AngleType angle_final = this->getAngle();
 
+    // Calculating average velocity
     return (angle_final - angle_initial) / (poll_duration / 1000.0);
 }
 
@@ -101,8 +197,56 @@ QuadratureEncoder::AngleType QuadratureEncoderClass::pollAngularVelocity(
  * angle of the encoder is in a known position, and angles should be reported
  * relative to that position. 
 */
-void QuadratureEncoderClass::resetAngleReference(
-    QuadratureEncoder::AngleType cur_angle = 0
+void QuadratureEncoder::resetAngleReference(
+    QuadratureEncoder::AngleType cur_angle
 ) {
     this->pulses = (int32_t) (cur_angle * this->epr);
 }
+
+#ifdef ISR_COMPILABLE
+/* To determine the direction of motion when a pulse starts or ends, we can
+ * consult the following truth tables:
+ * On change in A:
+ * A B | DIR
+ * 0 0 |  -
+ * 0 1 |  +
+ * 1 0 |  +
+ * 1 1 |  -
+ * 
+ * On change in B:
+ * A B | DIR
+ * 0 0 |  +
+ * 0 1 |  -
+ * 1 0 |  -
+ * 1 1 |  +
+ */ 
+
+// Namespace to contain some data used by the interrupts, shouldn't be
+// touched by user
+namespace __QuadratureEncoderISRData {
+    QuadratureEncoder& encoder = QuadratureEncoder::getInstance();
+    volatile int32_t* pulse_ctr = encoder.getPulseCountPtr();
+    uint8_t wire_x_pin_bm = encoder.pin_x_bm;
+    uint8_t wire_y_pin_bm = encoder.pin_y_bm;
+};
+
+// TODO: See what this gets compiled into, see if we need some custom ASM tuning
+ISR(ENCODER_SIGNAL_A_INT_VECTOR) {
+    uint8_t port_reading = ENCODER_SIGNAL_IN_PORT;
+    *__QuadratureEncoderISRData::pulse_ctr += (
+        port_reading & __QuadratureEncoderISRData::wire_x_pin_bm
+    ) ^ (
+        port_reading & __QuadratureEncoderISRData::wire_y_pin_bm
+    ) ? +1 : -1;
+}
+
+ISR(ENCODER_SIGNAL_B_INT_VECTOR) {
+    uint8_t port_reading = ENCODER_SIGNAL_IN_PORT;
+    *__QuadratureEncoderISRData::pulse_ctr += (
+        port_reading & __QuadratureEncoderISRData::wire_x_pin_bm
+    ) ^ (
+        port_reading & __QuadratureEncoderISRData::wire_y_pin_bm
+    ) ? -1 : +1;
+}
+
+#endif
--- src/QuadratureEncoder.hpp
+++ src/QuadratureEncoder.hpp
@@ -1,190 +1,55 @@
-#ifndef ENCODER_HPP
-#define ENCODER_HPP
+/**
+ * Despite the comments in this file, I am refraining from referring to it as a
+ * library as its' functionality is only guaranteed on an ATmega328p MCU.
+ * Additionally, it is only statically configurable and requires statically 
+ * defined configurations within the file. Since this makes it totally unfit
+ * for most reuse, I am simply using it as a project file.
+*/
+
+#ifndef QUADRATUREENCODER_HPP
+#define QUADRATUREENCODER_HPP
 
 #include <Arduino.h>
 
 /**
- * Currently this library is extremely inflexible with regards to different
- * physical wiring configurations and data collections. It only supports signal
- * A and B being connected to the same GPIO port, and it can only collect full
- * 4x-pulse resolution data (ie collecting rising and falling edges of A and B)
+ * Singleton class representing a connected quadrature encoder. See the 
+ * top of the .cpp definition file for configuration options.
 */
-
-// Unfortunately, because there's no good way to grab pin ports registers,
-// check their interrupt vectors, or generate ISRs (without Arduino's high-
-// latency `attachInterrupt()`), information on all of these must be passed to
-// the library via compile-time known `#define`s to ensure that this library
-// works quickly. It is the user's responsibility to ensure all related 
-// `#define`s are made correctly to ensure correct funtionality.
-//
-// For descriptions of each of these required `#defines`, see below.
-#if defined(ENCODER_SIGNAL_A_PIN)           && \
-    defined(ENCODER_SIGNAL_A_INT_VECTOR)    && \
-    defined(ENCODER_SIGNAL_B_PIN)           && \
-    defined(ENCODER_SIGNAL_B_INT_VECTOR)    && \
-    defined(ENCODER_SIGNAL_IN_PORT)
-
-#if digitalPinToInterrupt(ENCODER_SIGNAL_A_PIN) == NOT_AN_INTERRUPT
-    #error "Signal A pin must be interrupt-enabled."
-#endif
-#if digitalPinToInterrupt(ENCODER_SIGNAL_B_PIN) == NOT_AN_INTERRUPT
-    #error "Signal B pin must be interrupt-enabled."
-#endif
-
-#define ISR_COMPILABLE
-
-#else
-
-// `ENCODER_SIGNAL_A_PIN` should be the Arduino digital pin number of the
-// digital pin to which the "A" signal of the quadrature encoder is attached.
-// Example: `#define ENCODER_SIGNAL_A_PIN 2`
-#ifndef ENCODER_SIGNAL_A_PIN
-    #error "`ENCODER_WIRE_A_PIN` must be defined for library use."
-#endif
-
-// `ENCODER_SIGNAL_B_PIN` should be the Arduino digital pin number of the
-// digital pin to which the "B" signal of the quadrature encoder is attached.
-// Example: `#define ENCODER_SIGNAL_B_PIN 3`
-#ifndef ENCODER_SIGNAL_B_PIN
-    #error "`ENCODER_WIRE_B_PIN` must be defined for library use."
-#endif
-
-// `ENCODER_SIGNAL_A_INT_VECTOR` should be the interrupt vector of the on-change
-// interrupt of the digital pin to which the "A" signal of the quadrature
-// encoder is attached. This MUST be the interrupt vector for the corresponding
-// pin otherwise this library will not work.
-// Example: `#define ENCODER_SIGNAL_A_INT_VECTOR INT0_vect`
-#ifndef ENCODER_SIGNAL_A_INT_VECTOR
-    #error "`ENCODER_SIGNAL_A_INT_VECTOR must be defined for library use"
-
-#endif
-// `ENCODER_SIGNAL_B_INT_VECTOR` should be the interrupt vector of the on-change
-// interrupt of the digital pin to which the "B" signal of the quadrature
-// encoder is attached. This MUST be the interrupt vector for the corresponding
-// pin otherwise this library will not work.
-// Example: `#define ENCODER_SIGNAL_B_INT_VECTOR INT1_vect`
-#ifndef ENCODER_SIGNAL_B_INT_VECTOR
-    #error "`ENCODER_SIGNAL_B_INT_VECTOR must be defined for library use"
-#endif
-
-// `ENCODER_SIGNAL_IN_PORT` should be the Portx input register for the digital
-// pins to which both the "A" and "B" signals of the quadrature encoder are
-// attached. These two pins MUST be on the same port to allow the fastest
-// possible interrupt latency and avoid missing pulses. This MUST be the port to
-// which BOTH encoder signal wires are attached, otherwise this library will not
-// work.
-// Example: `#define ENCODER_SIGNAL_IN_PORT PIND`
-#ifndef ENCODER_SIGNAL_IN_PORT
-    #error "`ENCODER_SIGNAL_IN_PORT` must be defined for library use."
-#endif
-
-#endif
-
-// `ENCODER_PULSES_PER_REVOLUTION` should be the number of pulses generated per
-// revolution by the attached quadrature encoder. Note that this library uses 
-// rising and falling edges of the A and B signals of the encoder, meaning that
-// angular displacement will be measured 4x more frequently than the specified
-// PPR value.
-#ifndef ENCODER_PULSES_PER_REVOLUTION
-    #error "`ENCODER_PULSES_PER_REVOLUTION` must be defined for library use."
-#endif
-
-// `ENCODER_REVERSE` should be defined if the reported angular displacement of
-// the encoder should be inverted (+/- movement directions swapped).
-#ifdef ENCODER_REVERSE
-#define __ENCODER_SWAP_WIRES 1
-#else
-#define __ENCODER_SWAP_WIRES 0
-#endif
-
-namespace QuadratureEncoder {
+class QuadratureEncoder {
     typedef double AngleType;
 
-    namespace impl {
+    public:
         /**
-         * Simple class intended to encapsulate measurements taken from an npn-
-         * driven quadrature encoder.
+         * Grabs the singleton isntance of this class.
         */
-        class QuadratureEncoderClass {
-            public:
-                QuadratureEncoderClass(
-                    uint8_t pin_a,
-                    uint8_t pin_b,
-                    uint16_t pulses_per_revolution,
-                    bool invert_direction = false
-                );
+        static QuadratureEncoder& getInstance() {
+            static QuadratureEncoder instance;
 
-                AngleType getAngle();
-                AngleType pollAngularVelocity(uint32_t poll_duration);
-
-                void resetAngleReference(AngleType cur_angle = 0);
-
-                /** Getter for pointer to angle member. */
-                volatile int32_t* getPulseCountPtr()
-                { return &(this->pulses); };
-
-                uint8_t pin_x_bm;
-                uint8_t pin_y_bm;
-
-                protected:
-                    uint16_t ppr;
-                    double epr; // Double so we don't have to convert frequently
-                    volatile int32_t pulses;
+            return instance;
         };
-    };
 
-    // Initializing single QuadratureEncloderClass instance for use by user
-    QuadratureEncoder::impl::QuadratureEncoderClass Encoder(
-        ENCODER_SIGNAL_A_PIN,
-        ENCODER_SIGNAL_B_PIN,
-        __ENCODER_SWAP_WIRES
-    );
+        // Deleting the copy and assignment operators so no accidental instances
+        QuadratureEncoder(QuadratureEncoder const&) = delete;
+        void operator=(QuadratureEncoder const&)    = delete;
 
-    // Namespace to contain some data used by the interrupts, shouldn't be
-    // touched by user
-    namespace int_data {
-        volatile int32_t* pulse_ctr = Encoder.getPulseCountPtr();
-        uint8_t wire_x_pin_bm = Encoder.pin_x_bm;
-        uint8_t wire_y_pin_bm = Encoder.pin_x_bm;
-    };
+        AngleType getAngle();
+        AngleType pollAngularVelocity(uint32_t poll_duration);
+
+        void resetAngleReference(AngleType cur_angle = 0);
+
+        /** Getter for pointer to angle member. */
+        volatile int32_t* getPulseCountPtr()
+        { return &(this->pulses); };
+
+        uint8_t pin_x_bm;
+        uint8_t pin_y_bm;
+
+    protected:
+        QuadratureEncoder();
+
+        uint16_t ppr;
+        long double epr; // Double so we don't have to convert frequently
+        volatile int32_t pulses;
 };
-
-#ifdef ISR_COMPILABLE
-/* To determine the direction of motion when a pulse starts or ends, we can
- * consult the following truth tables:
- * On change in A:
- * A B | DIR
- * 0 0 |  -
- * 0 1 |  +
- * 1 0 |  +
- * 1 1 |  -
- * 
- * On change in B:
- * A B | DIR
- * 0 0 |  +
- * 0 1 |  -
- * 1 0 |  -
- * 1 1 |  +
- */ 
-
-// TODO: See what this gets compiled into, see if we need some custom ASM tuning
-ISR(ENCODER_SIGNAL_A_INT_VECTOR) {
-    uint8_t port_reading = ENCODER_SIGNAL_IN_PORT;
-    *QuadratureEncoder::int_data::pulse_ctr += (
-        port_reading & QuadratureEncoder::int_data::wire_x_pin_bm
-    ) ^ (
-        port_reading & QuadratureEncoder::int_data::wire_y_pin_bm
-    ) ? +1 : -1;
-}
-
-ISR(ENCODER_SIGNAL_B_INT_VECTOR) {
-    uint8_t port_reading = ENCODER_SIGNAL_IN_PORT;
-    *QuadratureEncoder::int_data::pulse_ctr += (
-        port_reading & QuadratureEncoder::int_data::wire_x_pin_bm
-    ) ^ (
-        port_reading & QuadratureEncoder::int_data::wire_y_pin_bm
-    ) ? -1 : +1;
-}
-#endif
 
 #endif
