fa9fc0303a926ad93d09fb0cfcfd4fbe013c00e8
Owen Grimm <grimm.owen@gmail.com>
2024-02-15 19:38:17 -0500

    Using encoder-lib provided velocity tracking

--- src/main.cpp
+++ src/main.cpp
@@ -14,24 +14,13 @@ void setup() {
 }
 
 // Smoothing
-unsigned long pre_sample_time = 0;
-double cur_angle = 0;
-double pre_angle = 0;
 const double VEL_SMOOTHER_HL = 0.02; // half life of smoothing (in s)
 Smoothing::Smoother vel_smoother(VEL_SMOOTHER_HL, 0);
 
 void loop() {
-  // TODO: Velocity calculation and smoothing
-  // These might be small enough to introduce rounding errors... we shall see
-  pre_angle = cur_angle;
-  cur_angle = encoder.getAngle();
-
-  unsigned long delta_micros = micros() - pre_sample_time;
-  pre_sample_time += delta_micros;
-  double delta_time = delta_micros / 1000000.0; // in seconds
-
-  double vel = (cur_angle - pre_angle) / delta_time;  // in rot/s
-  double smoothed_vel = vel_smoother.updateSmoothedValue(vel, delta_time);
+  double cur_angle = encoder.getAngle();
+  double vel = encoder.getAngularVelocity();
+  // double smoothed_vel = vel_smoother.updateSmoothedValue(vel, delta_time);
 
   // TODO: Control Loop
   
@@ -41,10 +30,10 @@ void loop() {
   Serial.println(cur_angle, 10);
   Serial.print(">rawvel:");
   Serial.println(vel);
-  Serial.print(">smoothvel:");
-  Serial.println(smoothed_vel);
-  Serial.print(">frametime:");
-  Serial.println(delta_time);
+  // Serial.print(">smoothvel:");
+  // Serial.println(smoothed_vel);
+  // Serial.print(">frametime:");
+  // Serial.println(delta_time);
 
   #if LOOP_MIN_PERIOD > 0
   delay(LOOP_MIN_PERIOD);
